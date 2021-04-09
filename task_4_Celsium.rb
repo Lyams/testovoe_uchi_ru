1cb3224eea422c38ca09a3d4b9cd6d9b3fbc8628
Ilya L <lyamsh@yandex.ru>
2021-04-09 06:09:32 +0300

    realise consol programm for 4 task (Celsium)

--- task_4_Celsium.rb
+++ task_4_Celsium.rb
@@ -2,6 +2,16 @@ def cels_to_fahrenheit(value)
   (9 * value) / 5.0 + 32.0
 end
 
-def gets_censium
-  
+def gets_celsium
+  puts 'Пожалуйста, введите градусы по Цельсию'
+  gets.chomp
+end
+loop do
+  c = gets_celsium.to_i
+  if c < -273.15
+    puts 'Температура не может быть ниже абсолютного нуля!'
+    next
+  end
+  puts "#{c} градусов по Цельсию будет равно #{cels_to_fahrenheit(c)} градусов по Фаренгейту"
+  break
 end
