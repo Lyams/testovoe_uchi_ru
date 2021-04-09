e4fe6d13ea099e4013633e2aa90124ea0bc55967
Ilya L <lyamsh@yandex.ru>
2021-04-09 05:15:35 +0300

    add solve in 3 task and fix 1 task

--- tasks_1_2_3.rb
+++ tasks_1_2_3.rb
@@ -1,8 +1,8 @@
 # Task 1
 arr1 = [1, 2, 12, 34, 35, 6, 0, 34, 122, 124, 789, 999, 33, 54, 763, 893]
 # simple way
-arr.max(2)
-arr.min(2)
+arr1.max(2)
+arr1.min(2)
 # Option with function
 class Array
   def max2
@@ -22,7 +22,7 @@ arr2 = [{a: 1, b: 2, c: 45}, {d: 123, c: 12}, {e: 87}]
 values_arr2 = arr2.map{ |hash| hash.values }.flatten
 keys_arr2 = arr2.map{ |hash| hash.keys }.flatten
 # if need uniq keys
-# uniq_keys = keys_arr2.uniq
+uniq_keys = keys_arr2.uniq
 summ_values = values_arr2.sum
 
 p values_arr2 
@@ -30,3 +30,8 @@ p keys_arr2
 p summ_values
 p uniq_keys
 
+# Task 3
+entry = Hash.new(0)
+arr3 = [ nil, 2, :foo, "bar", "foo", "apple", "orange", :orange, 45, nil, :foo, :bar, 25, 45, :apple, "bar", nil]
+arr3.map {|el| entry[el] +=1}
+p entry
