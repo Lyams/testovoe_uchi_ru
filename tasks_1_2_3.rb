c786e9198754069e35f69285ea0a1b83e12823a1
Ilya L <lyamsh@yandex.ru>
2021-04-09 05:21:57 +0300

    format style

--- tasks_1_2_3.rb
+++ tasks_1_2_3.rb
@@ -17,21 +17,21 @@ p arr1.max2
 p arr1.min2
 
 # Task 2
-arr2 = [{a: 1, b: 2, c: 45}, {d: 123, c: 12}, {e: 87}]
+arr2 = [{ a: 1, b: 2, c: 45 }, { d: 123, c: 12 }, { e: 87 }]
 
-values_arr2 = arr2.map{ |hash| hash.values }.flatten
-keys_arr2 = arr2.map{ |hash| hash.keys }.flatten
+values_arr2 = arr2.map { |hash| hash.values }.flatten
+keys_arr2 = arr2.map { |hash| hash.keys }.flatten
 # if need uniq keys
 uniq_keys = keys_arr2.uniq
 summ_values = values_arr2.sum
 
-p values_arr2 
+p values_arr2
 p keys_arr2
 p summ_values
 p uniq_keys
 
 # Task 3
 entry = Hash.new(0)
-arr3 = [ nil, 2, :foo, "bar", "foo", "apple", "orange", :orange, 45, nil, :foo, :bar, 25, 45, :apple, "bar", nil]
-arr3.map {|el| entry[el] +=1}
+arr3 = [nil, 2, :foo, 'bar', 'foo', 'apple', 'orange', :orange, 45, nil, :foo, :bar, 25, 45, :apple, 'bar', nil]
+arr3.map { |el| entry[el] += 1 }
 p entry
