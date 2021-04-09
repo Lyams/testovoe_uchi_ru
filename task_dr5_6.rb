04f1ae7e8c968438aea4e156043af05c86b1aec1
Ilya L <lyamsh@yandex.ru>
2021-04-10 02:55:41 +0300

    Realise connection with PostgreSQL

--- task_dr5_6.rb
+++ task_dr5_6.rb
@@ -1,1 +1,8 @@
+require 'pg'
 
+conn = PG.connect( dbname: 'postgres', user: 'ilya' , password: '1')
+
+conn.exec( "select COUNT(*) from students where name = 'Иван';" ) do
+  |result| puts 
+  result.each{ |row| puts row.values}
+end
