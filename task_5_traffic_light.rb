7d62dc2a0d5b6ea05d8322e69b1771a3790c38ae
Ilya L <lyamsh@yandex.ru>
2021-04-09 07:09:59 +0300

    obrabotka nekorektnih daniih in 5 task

--- task_5_traffic_light.rb
+++ task_5_traffic_light.rb
@@ -1,4 +1,16 @@
+require 'tty-prompt'
+
 def gets_color(input)
-    cmd = {"red" => "стоять", "green" => "идти", "yellow" => "ждать"}
-    cmd[input]
+  cmd = { 'red' => 'стоять', 'green' => 'идти', 'yellow' => 'ждать' }
+  cmd[input]
+end
+
+prompt = TTY::Prompt.new
+
+loop do
+  puts 'Ввведите цвет светофора'
+  action = gets_color(gets.chomp.downcase)
+  puts 'Некорректные данные, повторите ввод' if action.nil?
+  puts action
+  break if prompt.yes?('Хотите ли вы выйти?')
 end
