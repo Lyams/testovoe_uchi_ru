7d478a3a5d5aafc6cbe15b3025518fcfc516c9fe
Ilya L <lyamsh@yandex.ru>
2021-04-09 05:10:11 +0300

    add solve in 2 task

--- tasks_1_2_3.rb
+++ tasks_1_2_3.rb
@@ -15,3 +15,18 @@ class Array
 end
 p arr1.max2
 p arr1.min2
+
+# Task 2
+arr2 = [{a: 1, b: 2, c: 45}, {d: 123, c: 12}, {e: 87}]
+
+values_arr2 = arr2.map{ |hash| hash.values }.flatten
+keys_arr2 = arr2.map{ |hash| hash.keys }.flatten
+# if need uniq keys
+# uniq_keys = keys_arr2.uniq
+summ_values = values_arr2.sum
+
+p values_arr2 
+p keys_arr2
+p summ_values
+p uniq_keys
+
