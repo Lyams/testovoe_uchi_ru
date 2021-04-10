c4b92e5e27a960f5a63fd570532fe8d4aba48a98
Ilya L <lyamsh@yandex.ru>
2021-04-10 16:53:34 +0300

    define HOF for 1 task

--- tasks_1_2_3.rb
+++ tasks_1_2_3.rb
@@ -1,15 +1,15 @@
 # Task 1
 arr1 = [1, 2, 12, 34, 35, 6, 0, 34, 122, 124, 789, 999, 33, 54, 763, 893]
 # simple way
-arr1.max(2)
-arr1.min(2)
+p arr1.max(2)
+p arr1.min(2)
 
 # "Bicycle", algoritmic way
 def max_two(arr)
   m1= arr[0]
   m2 = arr[1]
   m2, m1 = m1, m2 if m2 > m1
-  arr.each do
+  arr[2..].each do
     |tmp|
 	if tmp > m1
 	then m2, m1 = m1, tmp
@@ -20,12 +20,35 @@ def max_two(arr)
   return [m1,m2]
 end
 
-  def min2
-    min(2)
+def order_two(arr, f)
+  m1= arr[0]
+  m2 = arr[1]
+  return [m1,nil] if m2.nil?
+  m2, m1 = m1, m2 if f.call(m2,m1)
+  arr[2..].each do |tmp|
+	if  f.call(tmp, m1)
+	then m2, m1 = m1, tmp
+	elsif f.call(tmp,m2)
+	then m2 = tmp
+	end
   end
+  return [m1,m2]
+end
+
+def min_2(arr)
+  sravni = -> (x , y) { x < y }
+  order_two(arr, sravni)
+end
+  
+def max_2(arr)
+  sravni = -> (x , y) { x > y }
+  order_two(arr, sravni)
+end
 
 p max_two(arr1)
 p arr1
+p min_2(arr1)
+p max_2(arr1)
 
 # Task 2
 arr2 = [{ a: 1, b: 2, c: 45 }, { d: 123, c: 12 }, { e: 87 }]
