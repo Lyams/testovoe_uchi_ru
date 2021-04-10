b23353b5c24af3a1293bf9b80f84341c71583864
Ilya L <lyamsh@yandex.ru>
2021-04-10 16:17:06 +0300

    Define 'bicycle' for max2 of task 1

--- tasks_1_2_3.rb
+++ tasks_1_2_3.rb
@@ -3,18 +3,29 @@ arr1 = [1, 2, 12, 34, 35, 6, 0, 34, 122, 124, 789, 999, 33, 54, 763, 893]
 # simple way
 arr1.max(2)
 arr1.min(2)
-# Option with function
-class Array
-  def max2
-    max(2)
+
+# "Bicycle", algoritmic way
+def max_two(arr)
+  m1= arr[0]
+  m2 = arr[1]
+  m2, m1 = m1, m2 if m2 > m1
+  arr.each do
+    |tmp|
+	if tmp > m1
+	then m2, m1 = m1, tmp
+	elsif tmp > m2
+	then m2 = tmp
+	end
   end
+  return [m1,m2]
+end
 
   def min2
     min(2)
   end
-end
-p arr1.max2
-p arr1.min2
+
+p max_two(arr1)
+p arr1
 
 # Task 2
 arr2 = [{ a: 1, b: 2, c: 45 }, { d: 123, c: 12 }, { e: 87 }]
