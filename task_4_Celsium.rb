a34b3ec875a3f76281c79e5b08ebb191ef843b82
Ilya L <lyamsh@yandex.ru>
2021-04-11 00:12:29 +0300

    add first variance for review'

--- task_4_Celsium.rb
+++ task_4_Celsium.rb
@@ -2,6 +2,7 @@ class Temperature
 # Неясна необходимая точность и будет ли температура
 # переводиться туда-сюда. Возможно имеет смысл
 # использовать BigDecimal из Std-lib вместо float
+# либо наоборот Integer
 def cels_to_fahrenheit(value)
   (9.0 * value) / 5.0 + 32.0
 end
@@ -44,3 +45,29 @@ end
 end
 
 Temperature.new.start 
+
+=begin Изначальный вариант был такой
+def cels_to_fahrenheit(value)
+  (9 * value) / 5.0 + 32.0
+end
+
+def gets_celsium
+  puts 'Пожалуйста, введите градусы по Цельсию'
+  gets.chomp
+end
+
+loop do
+  str = gets_celsium
+  unless str.match?(/-*\d+/)
+    puts 'Ошибка, надо вводить только числа без букв, отрицательные - допускаются'
+    next
+  end
+  c = str.to_f
+  if c < -273.15
+    puts 'Температура не может быть ниже абсолютного нуля!'
+    next
+  end
+  puts "#{c} градусов по Цельсию будет равно #{cels_to_fahrenheit(c)} градусов по Фаренгейту"
+  break
+end  
+=end
