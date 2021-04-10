ad941f0c7b69c0806ba995639f4f96ef9f143fec
Ilya L <lyamsh@yandex.ru>
2021-04-11 00:06:01 +0300

    Refactor 4 task: create class Temperature

--- task_4_Celsium.rb
+++ task_4_Celsium.rb
@@ -1,23 +1,46 @@
+class Temperature
+# Неясна необходимая точность и будет ли температура
+# переводиться туда-сюда. Возможно имеет смысл
+# использовать BigDecimal из Std-lib вместо float
 def cels_to_fahrenheit(value)
-  (9 * value) / 5.0 + 32.0
+  (9.0 * value) / 5.0 + 32.0
 end
+
+def start
+    str = gets_celsium
+    matches(str) ? (c = str.to_f) : raise
+    check_existence(c) ? pretty_p(c, cels_to_fahrenheit(c)) : raise
+  rescue 
+     retry
+end
+
+private 
 
 def gets_celsium
   puts 'Пожалуйста, введите градусы по Цельсию'
   gets.chomp
 end
 
-loop do
-  str = gets_celsium
-  unless str.match?(/-*\d+/)
+def  matches(s)
+  unless s.match?(/-*\d+/)
     puts 'Ошибка, надо вводить только числа без букв, отрицательные - допускаются'
-    next
+    return false
   end
-  c = str.to_f
-  if c < -273.15
-    puts 'Температура не может быть ниже абсолютного нуля!'
-    next
-  end
-  puts "#{c} градусов по Цельсию будет равно #{cels_to_fahrenheit(c)} градусов по Фаренгейту"
-  break
+  return true
 end
+
+def  check_existence(s)
+  if s < -273.15
+  puts 'Температура не может быть ниже абсолютного нуля!'
+  return false
+  end
+  return true
+end
+
+def pretty_p(cels, fahr)
+  puts "#{cels} градусов по Цельсию будет равно #{fahr.round(2)} градусов по Фаренгейту"
+end
+
+end
+
+Temperature.new.start 
