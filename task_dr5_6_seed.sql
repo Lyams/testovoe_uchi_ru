9c6f5b31266668af7ddf8656fe88e72431cc7279
Ilya L <lyamsh@yandex.ru>
2021-04-12 16:18:04 +0300

    FIX SQL-query (6c task)

--- task_dr5_6_seed.sql
+++ task_dr5_6_seed.sql
@@ -13,7 +13,8 @@ name varchar NOT NULL,
 created_at TIMESTAMP ,
 parent_id int,
 
-FOREIGN KEY (parent_id)  REFERENCES parents(id));
+--FOREIGN KEY (parent_id)  REFERENCES parents(id)
+);
 
 INSERT INTO parents (id, name,created_at)
 VALUES ('1', 'Ura', '2020-04-26 02:04:24' ), 
@@ -23,7 +24,7 @@ INSERT INTO students(id, name, created_at, parent_id )
 VALUES ('1', 'Иван', '2020-04-26 02:04:24', '1'),
 ('2', 'Masha', '2019-03-16 10:14:00', '2'),
 ('3', 'Anatolii', '2015-01-01 10:14:00', '2')
---('4', 'Ефим', '2021-01-01 10:14:00', NULL)
+('4', 'Ефим', '2021-01-01 10:14:00', NULL)
 ;
 
 
